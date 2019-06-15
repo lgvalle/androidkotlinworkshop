726c63a765eb7c274e0914f9c176505446fe5f0f
lgvalle <luis.gvalle@gmail.com>
2019-06-15 12:39:38 +0200

    Add basic ui binding

--- app/src/main/java/com/kotlinsevilla/cookietinder/MainActivity.kt
+++ app/src/main/java/com/kotlinsevilla/cookietinder/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.kotlinsevilla.cookietinder
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cookieName = findViewById<TextView>(R.id.cookieName)
+        val cookieDescription = findViewById<TextView>(R.id.cookieDescription)
+        val cookieImage = findViewById<ImageView>(R.id.cookieImage)
+        val likeButton = findViewById<Button>(R.id.likeButton)
+        val dislikeButton = findViewById<Button>(R.id.dislikeButton)
+
+        cookieName.text = "Oreo"
+        cookieDescription.text = "Likes sandwiches and chocolate"
+        cookieImage.setImageResource(R.drawable.cookie_oreo)
+
+        likeButton.setOnClickListener { Toast.makeText(this, "Like clicked!", Toast.LENGTH_SHORT).show() }
+        dislikeButton.setOnClickListener { Toast.makeText(this, "Dislike clicked!", Toast.LENGTH_SHORT).show() }
     }
 
 }
